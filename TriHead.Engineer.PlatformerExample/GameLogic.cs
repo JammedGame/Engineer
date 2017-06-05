28677ba3ced340981cac09ab54536a5b0b04fc72
manojlovicsmilos <manojlovicsmilos@hotmail.com>
2017-06-06 00:58:17 +0200

    Example Platformer 1

--- TriHead.Engineer.PlatformerExample/GameLogic.cs
+++ TriHead.Engineer.PlatformerExample/GameLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Engineer.Engine;
+using Engineer.Mathematics;
 using Engineer.Runner;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,7 @@ namespace Engineer.PlatformerExample
         private Scene2D _CurrentScene;
         private ExternRunner _Runner;
         private ResourceManager _ResMan;
+        private List<SceneObject> _Colliders;
         public Game CurrentGame
         {
             get
@@ -65,6 +67,26 @@ namespace Engineer.PlatformerExample
             _CurrentScene.Events.Extern.MouseClick += new GameEventHandler(MouseClickEvent);
             _CurrentScene.Events.Extern.MouseMove += new GameEventHandler(MouseMoveEvent);
             _CurrentScene.Events.Extern.TimerTick += new GameEventHandler(GameUpdateEvent);
+            _CurrentScene.BackColor = Color.FromArgb(41, 216, 238);
+            CreateFloor();
+        }
+        private void CreateFloor()
+        {
+            SpriteSet FloorSet = new SpriteSet("Idle");
+            FloorSet.Sprite.Add(global::Engineer.PlatformerExample.Properties.Resources.floor);
+            Sprite FloorSprite = new Sprite();
+            FloorSprite.SpriteSets.Add(FloorSet);
+            FloorSprite.Translation = new Vertex(FloorSprite.Translation.X, _WindowSize.Y - FloorSprite.Scale.Y, FloorSprite.Translation.Z);
+            DrawnSceneObject Floor = new DrawnSceneObject("Floor", FloorSprite);
+            for (int i = 0; i < 14; i++)
+            {
+                DrawnSceneObject FloorN = new DrawnSceneObject(Floor, _CurrentScene);
+                FloorN.Representation.Translation = new Vertex(FloorN.Representation.Translation.X + i * 98, FloorN.Representation.Translation.Y, FloorN.Representation.Translation.Z);
+                _CurrentScene.AddSceneObject(FloorN);
+            }
+            DrawnSceneObject FloorU = new DrawnSceneObject(Floor, _CurrentScene);
+            FloorU.Representation.Translation = new Vertex(FloorU.Representation.Translation.X + 8 * 98, FloorU.Representation.Translation.Y - 200, FloorU.Representation.Translation.Z);
+            _CurrentScene.AddSceneObject(FloorU);
         }
         private void KeyPressEvent(Game G, EventArguments E)
         {
--- TriHead.Engineer.PlatformerExample/Properties/Resources.Designer.cs
+++ TriHead.Engineer.PlatformerExample/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace Engineer.PlatformerExample.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap floor {
+            get {
+                object obj = ResourceManager.GetObject("floor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap floor2 {
+            get {
+                object obj = ResourceManager.GetObject("floor2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
