8ba817f6ed07b133582f37be1721b888f980a509
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2018-06-07 10:26:28 +0200

    Changes

---
+++ Engineer.Engine/Updated/DrawObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Engineer.Engine.Updated
+{
+    public enum DrawObjectType
+    {
+        Undefined = 0,
+        Image = 1,
+        Sprite = 2,
+        Tile = 3,
+        Light = 4,
+        Model = 5
+    }
+    class DrawObject
+    {
+        private bool _Active;
+        private bool _Fixed;
+        private bool _Visible;
+        private bool _Modified;
+        private Color _Paint;
+        public bool Fixed { get => _Fixed; set => _Fixed = value; }
+        public bool Active { get => _Active; set => _Active = value; }
+        public bool Visible { get => _Visible; set => _Visible = value; }
+        public bool Modified { get => _Modified; set => _Modified = value; }
+        public Color Paint { get => _Paint; set => _Paint = value; }
+        public DrawObject() : base()
+        {
+            this._Active = false;
+            this._Fixed = false;
+            this._Visible = false;
+            this._Modified = false;
+            this._Paint = Color.White;
+        }
+        public DrawObject(DrawObject Old)
+    }
+}
---
+++ Engineer.Engine/Updated/SceneObject.cs
@@ -0,0 +1,144 @@
+﻿using Engineer.Data;
+using Engineer.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Engineer.Engine.Updated
+{
+    public enum SceneObjectType
+    {
+        Undefined = 0,
+        Drawn = 1,
+        Script = 2,
+        Sound = 3,
+        Other = 4
+    }
+    public class SceneObject
+    {
+        private string _ID;
+        private string _Name;
+        private SceneObjectType _Type;
+        private Scene _ParentScene;
+        private EventsPackage _Events;
+        private Dictionary<string, object> _Data;
+        public string ID
+        {
+            get
+            {
+                return _ID;
+            }
+
+            set
+            {
+                _ID = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+
+            set
+            {
+                _Name = value;
+            }
+        }
+        public SceneObjectType Type
+        {
+            get
+            {
+                return _Type;
+            }
+
+            set
+            {
+                _Type = value;
+            }
+        }
+        public EventsPackage Events
+        {
+            get
+            {
+                return _Events;
+            }
+
+            set
+            {
+                _Events = value;
+            }
+        }
+        public Dictionary<string, object> Data
+        {
+            get
+            {
+                return _Data;
+            }
+
+            set
+            {
+                _Data = value;
+            }
+        }
+        public List<DictionaryEntry> IO_DataList
+        {
+            get
+            {
+                if (_Data == null) return null;
+                List<DictionaryEntry> Entries = new List<DictionaryEntry>(_Data.Count);
+                foreach (string Key in _Data.Keys)
+                {
+                    Entries.Add(new DictionaryEntry(Key, _Data[Key]));
+                }
+                return Entries;
+            }
+            set
+            {
+                Console.WriteLine("test");
+                foreach (DictionaryEntry Pair in value)
+                {
+                    if (!_Data.ContainsKey(Pair.Key))
+                    {
+                        _Data[Pair.Key] = Pair.Value;
+                    }
+                }
+            }
+        }
+        public SceneObject() : this(null)
+        {
+        }
+        public SceneObject(SceneObject Old)
+        {
+            if (Old != null)
+            {
+                this._ID = Guid.NewGuid().ToString();
+                this._Name = Old._Name;
+                this._Type = Old._Type;
+                this._Events = new EventsPackage();
+                this._Data = new Dictionary<string, object>(Old._Data);
+            }
+            else
+            {
+                this._Name = Name;
+                this._ID = Guid.NewGuid().ToString();
+                this._Type = SceneObjectType.Undefined;
+                this._Events = new EventsPackage();
+                this._Data = new Dictionary<string, object>();
+            }
+        }
+        public SceneObject(SceneObject Old, string Name) : this(Old)
+        {
+            this._Name = Name;
+        }
+        public SceneObject Copy()
+        {
+            return new SceneObject(this);
+        }
+    }
+}
